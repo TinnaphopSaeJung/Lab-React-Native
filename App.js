3af4c4871f8fc215cf9cf855d232c7a470684ac9
TinnaphopSaeJung <6210110484@psu.ac.th>
2022-08-17 22:18:05 +0700

    initial

--- App.js
+++ App.js
@@ -1,10 +1,14 @@
 import { StatusBar } from 'expo-status-bar';
+import React from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 
 export default function App() {
+  const doIt = () => {
+    console.log("Hello from console")
+  }
   return (
     <View style={styles.container}>
-      <Text>Hello World</Text>
+      <Text onPress={doIt}>Hello World</Text>
       <StatusBar style="auto" />
     </View>
   );
