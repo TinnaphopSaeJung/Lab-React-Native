b8567026417888ad9cad0f1d92c4dec802e50956
TinnaphopSaeJung <6210110484@psu.ac.th>
2022-08-25 18:39:36 +0700

    section 4 Connect

--- component/Weather.js
+++ component/Weather.js
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Text } from "react-native-web";
 import Forecast from "./Forecast";
 import { ImageBackground } from "react-native-web";
@@ -10,6 +10,25 @@ export default function Weather(props){
         description: 'description',
         temp: 0
     })
+
+    useEffect(() => {
+        console.log(`fetching data with zipCode = ${props.zipCode}`)
+        if (props.zipCode) {
+            fetch(`http://api.openweathermap.org/data/2.5/weather?q=${props.zipCode},th&units=metric&APPID=b7e0517b1e3e089978e65a7026e094e9`)
+                .then((response) => response.json())
+                .then((json) => {
+                    setForecastInfo({
+                        main: json.weather[0].main,
+                        description: json.weather[0].description,
+                        temp: json.main.temp
+                    });
+                })
+                .catch((error) => {
+                    console.warn(error);
+                });
+        }
+    }, [props.zipCode])
+    
     return (
         <ImageBackground source={require('../bg.jpg')} style={styles.backdrop}>
             <Text style={styles.textChallenge}>Zip Code is  {props.zipCode}.</Text>
