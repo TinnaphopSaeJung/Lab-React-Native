b9ad2b8861e302e2d0228d3ae116b7da0bd3f285
TinnaphopSaeJung <6210110484@psu.ac.th>
2022-08-26 19:08:35 +0700

    section 5 Router

--- App.js
+++ App.js
@@ -1,23 +1,19 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, View } from 'react-native';
-import Weather from './component/Weather';
-import Constants from 'expo-constants';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+
+const Stack = createStackNavigator(1)
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Weather zipCode="90110"/>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator>
+        <Stack.Screen name="Home" component={ZipCodeScreen}/>
+        <Stack.Screen name="Weather" component={WeatherScreen}/>
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {                //ในส่วนนี้ไม่ได้เปลี่ยนเป็น padding เนื่องจากผมทำในเว็บ ไม่ได้ทำในมือถือ Android ครับ
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center'
-  },
-});
+
